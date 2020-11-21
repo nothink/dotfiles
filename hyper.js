ac804fc87f6d9529e352f11aa9c8feaeec5396b7
nothink <gakongakon@gmail.com>
2020-11-21 16:36:31 +0900

    Add fonts 'meslo for powerline

--- hyper.js
+++ hyper.js
@@ -13,7 +13,7 @@ module.exports = {
 
     // font family with optional fallbacks
     fontFamily:
-      'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+      ' "Meslo LG M DZ Regular for Powerline", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
