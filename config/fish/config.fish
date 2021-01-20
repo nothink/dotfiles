2c6f375f4c170d51933d3a997ed5ca6d671ecd89
nothink <gakongakon@gmail.com>
2021-01-21 07:17:22 +0900

    Update fish variables

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,5 +1,9 @@
 
 source /usr/local/opt/asdf/asdf.fish
 set -g fish_user_paths "/usr/local/opt/unzip/bin" $fish_user_paths
-set -g -x CPPFLAGS "$CPPFLAGS -I /usr/local/opt/openssl/include"
-set -g -x LDFLAGS "$LDFLAGS -L /usr/local/opt/openssl/lib"
+
+set -g -x CPPFLAGS "$CPPFLAGS -I/usr/local/opt/openssl/include -I/usr/local/opt/zlib/include"
+set -g -x LDFLAGS "$LDFLAGS -L/usr/local/opt/openssl/lib -L/usr/local/opt/zlib/lib"
+
+set -g -x PKG_CONFIG_PATH "$PKG_CONFIG_PATH /usr/local/opt/zlib/lib/pkgconfig"
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
