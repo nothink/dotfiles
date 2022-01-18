5d832de6d88730369da5af2f93492e7a8845fc80
nothink-sbinc <ishii@springboard-inc.jp>
2022-01-18 16:03:22 +0900

    Fix CI Actions

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,29 +1,31 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 
-fish_add_path $HOME/.bin
+    fish_add_path $HOME/.bin
 
-set -x LANG ja_JP.UTF-8
-set -x EDITOR nano
-set -x PAGER bat
+    set -x LANG ja_JP.UTF-8
+    set -x EDITOR nano
+    set -x PAGER bat
 
-# aliases (every using)
-alias ls="ls -FG"
+    # aliases (every using)
+    alias ls="ls -FG"
 
-# TODO: use abbr fro utils
-# https://qiita.com/wataash/items/ab0a8b86b60e782f537f
+    # TODO: use abbr fro utils
+    # https://qiita.com/wataash/items/ab0a8b86b60e782f537f
 
-set -x DOCKER_BUILDKIT 1
-set -x COMPOSE_DOCKER_CLI_BUILD 1
+    set -x DOCKER_BUILDKIT 1
+    set -x COMPOSE_DOCKER_CLI_BUILD 1
 
-source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
+    source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc
 
-if test -d (brew --prefix)"/share/fish/completions"
-    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
-end
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+    end
 
-if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-end
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
+
+    source /usr/local/opt/asdf/libexec/asdf.fish
 
 end
